6310e43a2dbb58022888436d5fa9b660129aefd3
stian <stianbekkelund@hotmail.com>
2021-03-16 22:42:08 +0100

    added custom font & changes to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,24 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        blue: {
+          100: "",
+          200: "",
+          300: "",
+          400: "",
+          500: "",
+          600: "",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
